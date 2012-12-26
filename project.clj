86b00f42343017cbb9cb216fb8a5ff40a4c94841
Tadayoshi Sato <tadayosi@gmail.com>
2012-12-26 23:46:33 +0900

    AOT compilation enabled for the project

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject eopl-2nd-in-clojure "0.1.0-SNAPSHOT"
   :description "Essentials of Programming Languages exercised in Clojure"
   :url "https://github.com/tadayosi/eopl-2nd-in-clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :aot :all)
